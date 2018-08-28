16d9f451c7ce609c305fed97d07037c74b82ed94
unknown <abhinavtwr5@gmail.com>
2018-08-29 01:53:44 +0530

    commit #3 Chapter 7

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
+var TINY_EFFECT_CLASS = 'is-tiny';
 
 function setDetails(imageUrl, titleText){
   var detailImage=document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -30,6 +34,7 @@ function addThumbClickHandler(thumb){
 thumb.addEventListener('click', function (event) {
 event.preventDefault();
 setDetailsFromThumb(thumb);
+showDetails();
 });
 }
 
@@ -40,10 +45,37 @@ var thumbnailArray = [].slice.call(thumbnails);
 return thumbnailArray;
 }
 
+function hideDetails() {
+'use strict';
+document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+'use strict';
+document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+frame.classList.add(TINY_EFFECT_CLASS);
+setTimeout(function () {
+frame.classList.remove(TINY_EFFECT_CLASS);
+}, 50);
+}
+
+function addKeyPressHandler() {
+'use strict';
+document.body.addEventListener('keyup', function (event) {
+event.preventDefault();
+console.log(event.keyCode);
+if (event.keyCode === ESC_KEY) {
+hideDetails();
+}
+});
+}
+
 function initializeEvents() {
 'use strict';
 var thumbnails = getThumbnailsArray();
 thumbnails.forEach(addThumbClickHandler);
+addKeyPressHandler();
 }
 
 initializeEvents();
